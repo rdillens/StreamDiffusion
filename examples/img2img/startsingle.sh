915cdb72c2f7f5be084d219ebfea3b726f873dff
rdillens <remydillenseger@gmail.com>
2024-11-29 14:48:08 -0600

    add command line parameters

---
+++ examples/img2img/startsingle.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python single.py --input="../../images/inputs/selfie.jpg" --output="../../images/outputs/output-selfie003.png" --prompt="van gogh oil painting, portrait, photorealistic"
